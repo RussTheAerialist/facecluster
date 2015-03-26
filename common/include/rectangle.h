f47c3f5fe9a4f63d54cf11155135094a7ae900ba
Russell Hay <me@russellhay.com>
2015-03-25 18:49:34 -0700

    Rectangle::FromString

--- common/include/rectangle.h
+++ common/include/rectangle.h
@@ -9,6 +9,7 @@ namespace af {
         {
         public:
             Rectangle(int x=-1, int y=-1, int width=-1, int height=-1);
+            bool isNull() { return x == -1 || y == -1 || width == -1 || height == -1; }
             int x, y, width, height;
             
             std::string toString() const;
